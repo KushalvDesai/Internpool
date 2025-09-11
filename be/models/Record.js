abb470814b8d2bfa29bf1e5af646a8b2f0f1f8b5
Kushal Desai <kushal.desaiofficial@gmail.com>
2025-09-12 00:52:09 +0530

    add mongoose models

---
+++ be/models/Record.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const internshipRecordSchema = new mongoose.Schema({
+  student: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  studentID: { type: String, required: true },
+  faculty: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  facultyCode: { type: String, required: true },
+  semester: { type: String, required: true },
+  company: { type: mongoose.Schema.Types.ObjectId, ref: "Company", required: true },
+  stipend: { type: Number },
+  offerLetter: { type: String }, // Google Drive Link
+  completionCertificate: { type: String }, // Google Drive Link
+}, { timestamps: true });
+
+export const InternshipRecord = mongoose.model("InternshipRecord", internshipRecordSchema);
